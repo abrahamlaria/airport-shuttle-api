16c2ff55fdc24cf3ce36cac24b87fe4ed8bbb21a
Abraham Laria <abraham@abrahamlaria.com>
2018-02-19 10:08:02 -0800

    Added error handling app.js

--- app.js
+++ app.js
@@ -42,3 +42,23 @@ app.use((req, res, next) => {
 
 //Routes which should handle requests
 app.use('/bookings', bookingsRoutes);
+
+//Handles not found routes
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+//Handles any other errors
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+
+module.exports = app;
