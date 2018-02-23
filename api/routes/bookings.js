b66b7257ce66f652389bc1a5d3e0d613a9fe9e59
Abraham Laria <abraham@abrahamlaria.com>
2018-02-23 14:53:16 -0800

    Added comments

--- api/routes/bookings.js
+++ api/routes/bookings.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-//Import BookingsController.js
+//Import BookingsController.js.
 const BookingsController = require('../controllers/bookingsController');
 
-//Get all products
+//Get all products.
 router.get('/', BookingsController.bookins_get_all);
 
-//Create a new booking
+//Create a new booking.
 router.post('/', BookingsController.bookings_create_booking);
 
-//Get a single product by ID
+//Get a single product by ID.
 router.get('/:bookingId', BookingsController.bookings_get_booking);
 
-//Delete a booking by ID
+//Delete a booking by ID.
 router.delete('/:bookingId', BookingsController.bookings_delete_booking);
 
-//Update a booking by ID
+//Update a booking by ID.
 router.patch('/:bookingId', BookingsController.bookings_update_booking);
 
 module.exports = router;
