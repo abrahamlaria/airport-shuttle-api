0bfec4f70255b0e0badee39d56ba7aa525ed46ea
Abraham Laria <abraham@abrahamlaria.com>
2018-03-06 09:54:56 -0800

    Optimized email sending function

--- api/controllers/bookingsController.js
+++ api/controllers/bookingsController.js
@@ -1,16 +1,19 @@
 const Booking = require('../models/bookings');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 
 //Get all products
 exports.bookins_get_all = (req, res, next) => {
-    Booking.find()
-        .select('_id trip_details vehicle options contact_info comments payment_details booking_date update_date booking_status assigned_to')
+    Booking
+        .find()
+        .select('_id trip_details vehicle options contact_info comments payment_details booking_d' +
+                'ate update_date booking_status assigned_to')
         .exec()
         .then(reservations => {
             const response = {
                 count: reservations.length,
                 bookings: reservations.map(reservation => {
-                    return {                                             
+                    return {
                         _id: reservation._id,
                         trip_details: {
                             pickup_location: reservation.trip_details.pickup_location,
@@ -70,60 +73,78 @@ exports.bookins_get_all = (req, res, next) => {
                     }
                 })
             };
-            res.status(200).json(response);          
+            res
+                .status(200)
+                .json(response);
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
-                error: err
-            });
+            res
+                .status(500)
+                .json({error: err});
         });
 }
 
 //Get a single order by ID
 exports.bookings_get_booking = (req, res, next) => {
     const id = req.params.bookingId;
-    Booking.findById(id)
-        .select('_id trip_details vehicle options contact_info comments payment_details booking_date update_date booking_status assigned_to')
+    Booking
+        .findById(id)
+        .select('_id trip_details vehicle options contact_info comments payment_details booking_d' +
+                'ate update_date booking_status assigned_to')
         .exec()
-        .then(reservation => {           
+        .then(reservation => {
             if (reservation) {
-                res.status(200).json({
-                    booking: reservation,
-                    request: {
-                        type: 'GET',
-                        url: 'http://localhost:3000/bookings/'
-                    }
-                });
+                res
+                    .status(200)
+                    .json({
+                        booking: reservation,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/bookings/'
+                        }
+                    });
             } else {
-                res.status(404).json({
-                    message: 'No valid entry found for provided ID.'
-                });
-            }           
+                res
+                    .status(404)
+                    .json({message: 'No valid entry found for provided ID.'});
+            }
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({error: err});
+            res
+                .status(500)
+                .json({error: err});
         });
 }
 
 //Create a new booking
 exports.bookings_create_booking = (req, res, next) => {
 
-    //If the type of selected payment is Deposit calculate the 25% of the total price
-    const deposit_amount = price => req.body.payment_details.payment_type === "Deposit" ? Number((price * 25) / 100) : 0;
-    
-    //Rest the price minus the deposit and the remaining amount should be paid in the vehicle.
+    // If the type of selected payment is Deposit calculate the 25% of the total
+    // price
+    const deposit_amount = price => req.body.payment_details.payment_type === "Deposit"
+        ? Number((price * 25) / 100)
+        : 0;
+
+    // Rest the price minus the deposit and the remaining amount should be paid in
+    // the vehicle.
     const in_car_payment = price => {
         const deposit = deposit_amount(price);
-        return deposit > 0 ? Number(price - deposit) : 0;
+        return deposit > 0
+            ? Number(price - deposit)
+            : 0;
     }
 
     //If it is roundtrip then the return pickup location = arrival dropoff location
-    const rt_pickup_location = roundtrip => roundtrip ? req.body.trip_details.dropoff_location : "";
-    
+    const rt_pickup_location = roundtrip => roundtrip
+        ? req.body.trip_details.dropoff_location
+        : "";
+
     //If it is roundtrip then the return dropoff location = arrival pickup location
-    const rt_dropoff_location = roundtrip => roundtrip ? req.body.trip_details.pickup_location : "";
+    const rt_dropoff_location = roundtrip => roundtrip
+        ? req.body.trip_details.pickup_location
+        : "";
 
     const booking = new Booking({
         _id: new mongoose
@@ -191,8 +212,7 @@ exports.bookings_create_booking = (req, res, next) => {
     });
     booking
         .save()
-        .then(result => {          
-            console.log(result);
+        .then(result => {
             res
                 .status(201)
                 .json({
@@ -256,6 +276,8 @@ exports.bookings_create_booking = (req, res, next) => {
                         }
                     }
                 });
+            //Send email confirmation to the admin
+            BookingConfirmation(result);
         })
         .catch(err => {
             console.log(err);
@@ -281,7 +303,7 @@ exports.bookings_update_booking = (req, res, next) => {
         updateOps[ops.propFlight_Number] = ops.flight_numberValue;
         updateOps[ops.propPassengers_Count] = ops.passengers_countValue;
         updateOps[ops.propSuitcases_Count] = ops.suitcases_count;
-        updateOps[ops.propRoundtrip] = ops.roundtrip;     
+        updateOps[ops.propRoundtrip] = ops.roundtrip;
         updateOps[ops.propRt_Pickup_Location] = ops.rt_pickup_locationValue;
         updateOps[ops.propRt_Dropoff_Location] = ops.rt_dropoff_locationValue;
         updateOps[ops.propRt_Pickup_Date] = ops.rt_pickup_dateValue;
@@ -309,56 +331,183 @@ exports.bookings_update_booking = (req, res, next) => {
         updateOps[ops.propReceived] = ops.receivedValue;
         updateOps[ops.propScheduled] = ops.scheduledValue;
         updateOps[ops.propAssigned] = ops.assignedValue;
-        updateOps[ops.propCompleted] = ops.completedValue;  
+        updateOps[ops.propCompleted] = ops.completedValue;
         //Update assigned to
-        updateOps[ops.propAssignedTo] =  ops.assignedToValue;
-    }  
-     //updateOps is an object that will have the updated value/values for name and price.
-    Booking.update({_id: id}, {$set: updateOps})
+        updateOps[ops.propAssignedTo] = ops.assignedToValue;
+    }
+    // updateOps is an object that will have the updated value/values for name and
+    // price.
+    Booking.update({
+        _id: id
+    }, {$set: updateOps})
         .exec()
-        .then( result => {
+        .then(result => {
             console.log(result);
-            res.status(200).json({
-                message: 'Booking updated',
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:3000/bookings/' + id
-                }
-            });
+            res
+                .status(200)
+                .json({
+                    message: 'Booking updated',
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/bookings/' + id
+                    }
+                });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });       
+            res
+                .status(500)
+                .json({error: err});
+        });
 }
 
 //Delete a product by ID
 exports.bookings_delete_booking = (req, res, next) => {
     const id = req.params.bookingId;
-    Booking.findByIdAndRemove({
-        _id: id
-    })
+    Booking
+        .findByIdAndRemove({_id: id})
         .exec()
-        .then( result => {
+        .then(result => {
             console.log(result);
-            res.status(200).json({
-                message: 'Product deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http://localhost:3000/products/',
-                    body: {
-                        name: 'String',
-                        price: 'Number'
+            res
+                .status(200)
+                .json({
+                    message: 'Product deleted',
+                    request: {
+                        type: 'POST',
+                        url: 'http://localhost:3000/products/',
+                        body: {
+                            name: 'String',
+                            price: 'Number'
+                        }
                     }
-                }
-            });
+                });
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
-                error: err
-            });
+            res
+                .status(500)
+                .json({error: err});
         });
+}
+
+//Send booking confirmation to the admin
+function BookingConfirmation(result) {
+    let roundtrip = "";
+    if (result.trip_details.roundtrip) {
+        roundtrip = `
+        <h3>Roundtrip</h3>
+        <ul> 
+        <li>Is Roundtrip: ${result.trip_details.roundtrip}</li>                  
+            <li>Pickup location: ${result.trip_details.rt_pickup_location}</li>
+            <li>Dropoff location: ${result.trip_details.rt_dropoff_location}</li>
+            <li>Pickup Date: ${result.trip_details.rt_pickup_date}</li>
+            <li>Pickup Time: ${result.trip_details.rt_pickup_time}</li> 
+            <li>Flight number: ${result.trip_details.rt_flight_number}</li>                      
+        </ul>
+    `;
+    }
+
+    let payment = "";
+    if (result.payment_details.payment_type === 'Full') {
+        payment = `
+        <h3>Payment details</h3>
+        <ul> 
+            <li>Payment type: ${result.payment_details.payment_type}</li>
+            <li>Payment method: ${result.payment_details.payment_method}</li>
+            <li>Discount code: ${result.payment_details.discount_code}</li>
+            <li>Total price: ${result.payment_details.total_price}</li>                      
+        </ul>
+    `;
+    }
+    else if (result.payment_details.payment_type === 'Deposit') {
+        payment = `
+        <h3>Payment details</h3>
+        <ul> 
+            <li>Payment type: ${result.payment_details.payment_type}</li>
+            <li>Payment method: ${result.payment_details.payment_method}</li>
+            <li>Discount code: ${result.payment_details.discount_code}</li>
+            <li>Total price: ${result.payment_details.total_price}</li>
+            <li>Deposit amount: ${result.payment_details.deposit_amount}</li>
+            <li>In car payment: ${result.payment_details.in_car_payment}</li>                   
+        </ul>
+    `;
+    }
+
+    const output = `
+        <p>You have a new booking from the website</p>
+        <h3>Contact Details</h3>
+        <ul>
+            <li>Name: ${result.contact_info.name}</li>
+            <li>Email: ${result.contact_info.email}</li>
+            <li>Phone: ${result.contact_info.phone_number}</li>
+            <li>Country: ${result.contact_info.country}</li>
+        </ul>
+        <h3>Trip details</h3>
+        <ul>
+            <li>Pickup location: ${result.trip_details.pickup_location}</li>
+            <li>Dropoff location: ${result.trip_details.dropoff_location}</li>
+            <li>Pickup date: ${result.trip_details.pickup_date}</li>
+            <li>Pickup time: ${result.trip_details.pickup_time}</li>
+            <li>Flight number: ${result.trip_details.flight_number}</li>
+            <li>Passengers count: ${result.trip_details.passengers_count}</li>
+            <li>Suitcases count: ${result.trip_details.suitcases_count}</li> 
+        </ul>
+        ` + roundtrip + `
+        <h3>Vehicle</h3>
+        <ul>                   
+            <li>Selected vehicle: ${result.vehicle.selected_vehicle}</li>
+        </ul>
+        <h3>Options</h3>
+        <ul>
+            <li>Baby seats: ${result.options.baby_seats}</li>
+            <li>Booster seats: ${result.options.booster_seats}</li>
+            <li>Special luggage: ${result.options.special_luggage}</li>
+            <li>Pets: ${result.options.pets}</li>
+            <li>Comments: ${result.comments}</li>
+        </ul>
+        ` + payment + `
+
+    `;
+    //Send email
+    SendMail(output);
+};
+
+function SendMail(output) {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        //host: 'smtp.google.com', port: 587,
+        service: 'gmail',
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'admin@comfortransfer.com', // Should be an environment variable
+            pass: 'Remember-2507' // Should be an environment variable
+        },
+        tls: { //Remove before going live
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Web Submission" <admin@comfortransfer.com>', // sender address
+        to: 'admin@comfortransfer.com, a_laria@yahoo.com', // list of receivers
+        subject: 'New booking ✔', // Subject line
+        text: '', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        //Do something after the email is sent. Redirect or re render the form.
+    });
+
+    //console.log(output + " This is the result object");
 }
--- api/models/bookings.js
+++ api/models/bookings.js
@@ -11,11 +11,11 @@ const bookingSchema = mongoose.Schema({
         passengers_count: {type: Number, required: true},
         suitcases_count: {type: Number, required: true},
         roundtrip: {type: Boolean},
-        rt_pickup_location: {type: String, required: true},
-        rt_dropoff_location: {type: String, required: true},
-        rt_pickup_date: {type: String, required: true},
-        rt_pickup_time: {type: String, required: true},       
-        rt_flight_number: {type: String, required: true}
+        rt_pickup_location: {type: String},
+        rt_dropoff_location: {type: String},
+        rt_pickup_date: {type: String},
+        rt_pickup_time: {type: String},       
+        rt_flight_number: {type: String}
     },
     vehicle: {
         selected_vehicle: {type: String, required: true},
@@ -41,12 +41,12 @@ const bookingSchema = mongoose.Schema({
     send_communications: {type: Boolean, required: true},
     agree_to_terms: {type: Boolean, required: true},
     payment_details: {
-        payment_type: {type: String, required: true},
-        payment_method: {type: String, required: true},
+        payment_type: {type: String},
+        payment_method: {type: String},
         discount_code: {type: String},
-        deposit_amount: {type: Number, required: true},
-        total_price: {type: Number, required: true},
-        in_car_payment: {type: Number, required: true}
+        deposit_amount: {type: Number},
+        total_price: {type: Number},
+        in_car_payment: {type: Number}
     },
     booking_date: {type: Date, required: true},
     update_date: {type: Date, required: true},
