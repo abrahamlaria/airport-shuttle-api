2f02c2c7727a8a9f87a032921ffdd79903ff59ef
Abraham Laria <abraham@abrahamlaria.com>
2018-03-10 18:37:41 -0800

    Modified error message

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {           
-            message: error.message + "This is an error"
+            message: error.message + "It worked"
         }
     });
 });
