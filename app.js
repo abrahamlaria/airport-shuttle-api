f56b11aa9a3e972ca8f1ce28ffec7737910da4c2
Abraham Laria <abraham@abrahamlaria.com>
2018-03-10 22:10:21 -0800

    Testing changes in app.js

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {           
-            message: error.message + " 12345"
+            message: error.message
         }
     });
 });
